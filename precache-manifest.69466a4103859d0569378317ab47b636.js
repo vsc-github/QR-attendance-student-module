3f6d5d431110370dab35e412fcb1896182b94219
vishwas <batty.wayn3@gmail.com>
2019-04-15 19:28:02 +0530

    new build for dynamic roll number updation

---
+++ precache-manifest.69466a4103859d0569378317ab47b636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/QR-attendance-student-module/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d3ef4c8823e67ae2081f",
+    "url": "/QR-attendance-student-module/static/js/runtime~main.f421feec.js"
+  },
+  {
+    "revision": "f3d3d904d05f21d5c0fd",
+    "url": "/QR-attendance-student-module/static/js/main.82ce825c.chunk.js"
+  },
+  {
+    "revision": "f3746bb744f36bb55854",
+    "url": "/QR-attendance-student-module/static/js/2.fe5d3d8f.chunk.js"
+  },
+  {
+    "revision": "f3d3d904d05f21d5c0fd",
+    "url": "/QR-attendance-student-module/static/css/main.2151ae4d.chunk.css"
+  },
+  {
+    "revision": "50135917a1f700a071031319fce4689e",
+    "url": "/QR-attendance-student-module/index.html"
+  }
+];
